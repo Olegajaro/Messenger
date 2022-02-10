e36b00f0725980348d0db6e7e88432fc6eff24a3
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-02-10 21:03:02 +0300

    ui: Connecting UI elements to code

--- Messenger/LoginViewController.swift
+++ Messenger/LoginViewController.swift
@@ -11,10 +11,42 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    // labels
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var repeatPasswordLabel: UILabel!
+    @IBOutlet weak var signUpLabel: UILabel!
+    
+    // textFields
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    // buttons
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var resendEmailButton: UIButton!
+    
+    // views
+    @IBOutlet weak var repeatPasswordLineView: UIView!
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
+    // MARK: - IBActions
+    @IBAction func loginButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func forgotPasswordButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func resendEmailButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+    }
 }
 
