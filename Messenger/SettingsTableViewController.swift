bb06a76c43005c19aba259444dd5c14fdc2ad9c1
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-02-14 19:03:12 +0300

    ui: Settings view setup

---
+++ Messenger/SettingsTableViewController.swift
@@ -0,0 +1,75 @@
+//
+//  SettingsTableViewController.swift
+//  Messenger
+//
+//  Created by Олег Федоров on 14.02.2022.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var appVersionLabel: UILabel!
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+        tableView.sectionHeaderTopPadding = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        showUserInfo()
+    }
+    
+    // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView,
+                            viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor(named: "TableViewBackgroundColor")
+        
+        return headerView
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            heightForHeaderInSection section: Int) -> CGFloat {
+        return section == 0 ? 0.0 : 10.0
+    }
+
+    // MARK: - IBActions
+    @IBAction func tellAFriendButtonPressed(_ sender: Any) {
+        print("DEBUG: tell a friend")
+    }
+    
+    @IBAction func termsAndConditionsButtonPressed(_ sender: Any) {
+        print("DEBUG: show T&C")
+    }
+    
+    @IBAction func logOutButtonPressed(_ sender: Any) {
+        print("DEBUG: log out")
+    }
+    
+    // MARK: - UpdateUI
+    private func showUserInfo() {
+        if let user = User.currentUser {
+            let versionApp = Bundle.main.infoDictionary?[
+                "CFBundleShortVersionString"
+            ] as? String ?? ""
+            
+            userNameLabel.text = user.userName
+            statusLabel.text = user.status
+            appVersionLabel.text = "App version \(versionApp)"
+            
+            if user.avatarLink != "" {
+                // download and set avatar image
+            }
+        }
+    }
+}
